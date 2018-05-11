f76ca755f1939c7506114121fc9fcf2bb2e692e0
paul <paul@findata.com.tw>
2018-05-11 12:10:11 +0800

    fix the react state problem

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,22 +1,36 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
+const redis = require('redis');
+const util = require('util');
 
 const Blog = mongoose.model('Blog');
 
-module.exports = app => {
+module.exports = (app) => {
   app.get('/api/blogs/:id', requireLogin, async (req, res) => {
     const blog = await Blog.findOne({
       _user: req.user.id,
-      _id: req.params.id
+      _id: req.params.id,
     });
 
     res.send(blog);
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
-    const blogs = await Blog.find({ _user: req.user.id });
+    const client = redis.createClient('redis://127.0.0.1:6379');
+    client.get = util.promisify(client.get);
 
-    res.send(blogs);
+    // do we have any cached data in our cached layer(redis) if yes,
+    // then response it, if not go to mlab
+    const cachedBlog = await client.get(req.user.id);
+    if (cachedBlog) {
+      // console.log('serving on redis');
+      return res.send(JSON.parse(cachedBlog));
+    }
+    const blogs = await Blog.find({ _user: req.user.id });
+    // console.log('serving on mongo db');
+    client.set(req.user.id, JSON.stringify(blogs)); // set data to redis
+
+    return res.send(blogs);
   });
 
   app.post('/api/blogs', requireLogin, async (req, res) => {
@@ -25,7 +39,7 @@ module.exports = app => {
     const blog = new Blog({
       title,
       content,
-      _user: req.user.id
+      _user: req.user.id,
     });
 
     try {
